2cea041a1d86701121f9020052b678feb99383ac
Samet Can Akcaalan <sametcan.akcaalan7@gmail.com>
2025-01-27 18:34:19 +0300

    solve image problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "avatars.githubusercontent.com" }],
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
   },
 };
 
